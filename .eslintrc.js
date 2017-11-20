baa38ce2a964b44fb5de24a794f3be084c995084
Rubens Mariuzzo <rubens@mariuzzo.com>
2017-11-20 13:47:05 -0400

    Basic React app.

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "prettier"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "jsx": true
+    },
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react",
+    "prettier"
+  ],
+  "rules": {
+    "prettier/prettier": "error"
+  }
+};
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const Html = require('html-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack')
+const Define = webpack.DefinePlugin
+const CommonsChunk = webpack.optimize.CommonsChunkPlugin
+const NamedModules = webpack.NamedModulesPlugin
+const HotModuleReplacement = webpack.HotModuleReplacementPlugin
+
+const paths = {
+  app: path.join(__dirname, './src/client/app/App.js'),
+  index: path.join(__dirname, './src/client/index.html'),
+  public: path.join(__dirname, './public'),
+  client: path.join(__dirname, './src/client')
+}
+
+module.exports = {
+  entry: {
+    app: [paths.app]
+  },
+  output: {
+    path: paths.public,
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  resolve: {
+    alias: {
+      '~': paths.client
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader', options: { importLoaders: true, modules: true } },
+          { loader: 'sass-loader' }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new Define({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new Html({
+      template: paths.index,
+      inject: 'body',
+      hash: true,
+      filename: 'index.html'
+    }),
+    new CommonsChunk({
+      name: 'vendor',
+      minChunks: (module) => /node_modules/.test(module.context)
+    }),
+    new HotModuleReplacement(),
+    new NamedModules(),
+  ],
+  devServer: {
+    historyApiFallback: true
+  }
+}
