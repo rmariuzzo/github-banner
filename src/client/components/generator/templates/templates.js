ab67e0ded3da7a77cb18c8734a4a88d575658fdb
Rubens Mariuzzo <rubens@mariuzzo.com>
2017-11-21 12:02:11 -0400

    Removed Apex template to avoid IP conflicts.
    @tj, if you ever see this, I want to apologize for using the public SVG
    that I found in your GitHub repository as not only the inspiration but
    also the base for the initial version.
    
    With this commit, I'm removing it from my public repository as well.
    
    Cheers!

--- src/client/components/generator/templates/templates.js
+++ src/client/components/generator/templates/templates.js
@@ -1,5 +1,4 @@
 export default [
-  require('./apex/Apex').default,
   require('./kira/Kira').default,
   require('./reflection/Reflection').default,
   require('./night-gradient/NightGradient').default
